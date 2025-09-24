01a61a7fd1a80d8ed1249bc35a3168c660f41217
Lê Kim Bảo Nhật <lekimbaonhat.92.2017@gmail.com>
2025-09-25 00:36:30 +0700

    feat: update landing header and routing; implement sign-up form

--- src/components/layout/data/landing-header-data.ts
+++ src/components/layout/data/landing-header-data.ts
@@ -2,23 +2,11 @@ import type { LandingHeader } from "../types";
 
 export const landingHeaderData: LandingHeader[] = [
     {
-        title: 'Home',
+        title: 'All Projects',
         url: '/'
     },
     {
-        title: 'Talent Pool',
-        url: '/talent-pool'
-    },
-    {
-        title: 'Marketplace',
-        url: '/marketplace'
-    },
-    {
-        title: 'For Enterprises',
-        url: '/for-enterprises'
-    },
-    {
-        title: 'Contact',
-        url: '/contact'
+        title: 'Companies',
+        url: '/companies'
     },
 ]
