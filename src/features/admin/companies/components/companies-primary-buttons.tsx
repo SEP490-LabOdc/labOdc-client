5cb25f09090c2b91d0d43db8173798966009cf89
annguyenbinh03 <phaolobinhan@gmail.com>
2025-10-04 12:44:34 +0700

    feat: add form create company

--- src/features/admin/companies/components/companies-primary-buttons.tsx
+++ src/features/admin/companies/components/companies-primary-buttons.tsx
@@ -1,12 +1,16 @@
 import { UserPlus } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { useCompanies } from './companies-provider'
+import { useNavigate } from '@tanstack/react-router';
 
 export function CompaniesPrimaryButtons() {
-    const { setOpen } = useCompanies()
+    const { setOpen } = useCompanies();
+
+    const navigate = useNavigate();
+
     return (
         <div className='flex gap-2'>
-            <Button className='space-x-1' onClick={() => setOpen('add')}>
+            <Button className='space-x-1' onClick={() => navigate({ to: '/admin/companies/create' })}>
                 <span>Tạo công ty</span> <UserPlus size={18} />
             </Button>
         </div>
