adc16b504682ebb81f076a6c1c08fa0052d68915
Lê Kim Bảo Nhật <lekimbaonhat.92.2017@gmail.com>
2025-11-03 22:02:46 +0700

    feat: add local data fetching for provinces and wards with error handling

---
+++ src/hooks/api/external/locations/data.ts
@@ -0,0 +1,133 @@
+export const allProvincesData = [
+  { "name": "Thành phố Hà Nội", "code": 1, "division_type": "thành phố trung ương", "codename": "ha_noi", "phone_code": 24, "wards": [] },
+  { "name": "Cao Bằng", "code": 4, "division_type": "tỉnh", "codename": "cao_bang", "phone_code": 206, "wards": [] },
+  { "name": "Tuyên Quang", "code": 8, "division_type": "tỉnh", "codename": "tuyen_quang", "phone_code": 207, "wards": [] },
+  { "name": "Điện Biên", "code": 11, "division_type": "tỉnh", "codename": "dien_bien", "phone_code": 215, "wards": [] },
+  { "name": "Lai Châu", "code": 12, "division_type": "tỉnh", "codename": "lai_chau", "phone_code": 213, "wards": [] },
+  { "name": "Sơn La", "code": 14, "division_type": "tỉnh", "codename": "son_la", "phone_code": 212, "wards": [] },
+  { "name": "Lào Cai", "code": 15, "division_type": "tỉnh", "codename": "lao_cai", "phone_code": 214, "wards": [] },
+  { "name": "Thái Nguyên", "code": 19, "division_type": "tỉnh", "codename": "thai_nguyen", "phone_code": 208, "wards": [] },
+];
+
+export const wardsData = [
+  {
+    provinceCode: '1',
+    wards: [
+      { "name": "Phường Ba Đình", "code": 4, "division_type": "phường", "codename": "phuong_ba_dinh", "province_code": 1 },
+      { "name": "Phường Ngọc Hà", "code": 8, "division_type": "phường", "codename": "phuong_ngoc_ha", "province_code": 1 },
+      { "name": "Phường Giảng Võ", "code": 25, "division_type": "phường", "codename": "phuong_giang_vo", "province_code": 1 },
+      { "name": "Phường Hoàn Kiếm", "code": 70, "division_type": "phường", "codename": "phuong_hoan_kiem", "province_code": 1 },
+      { "name": "Phường Cửa Nam", "code": 73, "division_type": "phường", "codename": "phuong_cua_nam", "province_code": 1 },
+      { "name": "Phường Phú Thượng", "code": 91, "division_type": "phường", "codename": "phuong_phu_thuong", "province_code": 1 },
+      { "name": "Phường Hồng Hà", "code": 97, "division_type": "phường", "codename": "phuong_hong_ha", "province_code": 1 },
+      { "name": "Phường Tây Hồ", "code": 103, "division_type": "phường", "codename": "phuong_tay_ho", "province_code": 1 },
+      { "name": "Phường Việt Hưng", "code": 127, "division_type": "phường", "codename": "phuong_viet_hung", "province_code": 1 },
+      { "name": "Phường Bồ Đề", "code": 130, "division_type": "phường", "codename": "phuong_bo_de", "province_code": 1 }
+    ]
+  },
+  {
+    provinceCode: '4',
+    wards: [
+      { "name": "Phường Thục Phán", "code": 1273, "division_type": "phường", "codename": "phuong_thuc_phan", "province_code": 4 },
+      { "name": "Phường Nùng Trí Cao", "code": 1279, "division_type": "phường", "codename": "phuong_nung_tri_cao", "province_code": 4 },
+      { "name": "Phường Tân Giang", "code": 1288, "division_type": "phường", "codename": "phuong_tan_giang", "province_code": 4 },
+      { "name": "Xã Bảo Lâm", "code": 1290, "division_type": "xã", "codename": "xa_bao_lam", "province_code": 4 },
+      { "name": "Xã Lý Bôn", "code": 1294, "division_type": "xã", "codename": "xa_ly_bon", "province_code": 4 },
+      { "name": "Xã Nam Quang", "code": 1297, "division_type": "xã", "codename": "xa_nam_quang", "province_code": 4 },
+      { "name": "Xã Quảng Lâm", "code": 1304, "division_type": "xã", "codename": "xa_quang_lam", "province_code": 4 },
+      { "name": "Xã Yên Thổ", "code": 1318, "division_type": "xã", "codename": "xa_yen_tho", "province_code": 4 },
+      { "name": "Xã Bảo Lạc", "code": 1321, "division_type": "xã", "codename": "xa_bao_lac", "province_code": 4 },
+      { "name": "Xã Cốc Pàng", "code": 1324, "division_type": "xã", "codename": "xa_coc_pang", "province_code": 4 },
+    ]
+  },
+  {
+    provinceCode: '8',
+    wards: [
+      { "name": "Phường Hà Giang 2", "code": 691, "division_type": "phường", "codename": "phuong_ha_giang_2", "province_code": 8 },
+      { "name": "Phường Hà Giang 1", "code": 694, "division_type": "phường", "codename": "phuong_ha_giang_1", "province_code": 8 },
+      { "name": "Xã Ngọc Đường", "code": 700, "division_type": "xã", "codename": "xa_ngoc_duong", "province_code": 8 },
+      { "name": "Xã Phú Linh", "code": 706, "division_type": "xã", "codename": "xa_phu_linh", "province_code": 8 },
+      { "name": "Xã Lũng Cú", "code": 715, "division_type": "xã", "codename": "xa_lung_cu", "province_code": 8 },
+      { "name": "Xã Đồng Văn", "code": 721, "division_type": "xã", "codename": "xa_dong_van", "province_code": 8 },
+      { "name": "Xã Sà Phìn", "code": 733, "division_type": "xã", "codename": "xa_sa_phin", "province_code": 8 },
+      { "name": "Xã Phố Bảng", "code": 745, "division_type": "xã", "codename": "xa_pho_bang", "province_code": 8 },
+      { "name": "Xã Lũng Phìn", "code": 763, "division_type": "xã", "codename": "xa_lung_phin", "province_code": 8 },
+      { "name": "Xã Mèo Vạc", "code": 769, "division_type": "xã", "codename": "xa_meo_vac", "province_code": 8 },
+    ]
+  },
+  {
+    provinceCode: '11',
+    wards: [
+      { "name": "Phường Điện Biên Phủ", "code": 3127, "division_type": "phường", "codename": "phuong_dien_bien_phu", "province_code": 11 },
+      { "name": "Phường Mường Lay", "code": 3151, "division_type": "phường", "codename": "phuong_muong_lay", "province_code": 11 },
+      { "name": "Xã Sín Thầu", "code": 3158, "division_type": "xã", "codename": "xa_sin_thau", "province_code": 11 },
+      { "name": "Xã Mường Nhé", "code": 3160, "division_type": "xã", "codename": "xa_muong_nhe", "province_code": 11 },
+      { "name": "Xã Nậm Kè", "code": 3162, "division_type": "xã", "codename": "xa_nam_ke", "province_code": 11 },
+      { "name": "Xã Mường Toong", "code": 3163, "division_type": "xã", "codename": "xa_muong_toong", "province_code": 11 },
+      { "name": "Xã Quảng Lâm", "code": 3164, "division_type": "xã", "codename": "xa_quang_lam", "province_code": 11 },
+      { "name": "Xã Mường Chà", "code": 3166, "division_type": "xã", "codename": "xa_muong_cha", "province_code": 11 },
+      { "name": "Xã Nà Hỳ", "code": 3169, "division_type": "xã", "codename": "xa_na_hy", "province_code": 11 },
+      { "name": "Xã Na Sang", "code": 3172, "division_type": "xã", "codename": "xa_na_sang", "province_code": 11 },
+    ]
+  },
+  {
+    provinceCode: '12',
+    wards: [
+      { "name": "Phường Đoàn Kết", "code": 3388, "division_type": "phường", "codename": "phuong_doan_ket", "province_code": 12 },
+      { "name": "Xã Bình Lư", "code": 3390, "division_type": "xã", "codename": "xa_binh_lu", "province_code": 12 },
+      { "name": "Xã Sin Suối Hồ", "code": 3394, "division_type": "xã", "codename": "xa_sin_suoi_ho", "province_code": 12 },
+      { "name": "Xã Tả Lèng", "code": 3405, "division_type": "xã", "codename": "xa_ta_leng", "province_code": 12 },
+      { "name": "Phường Tân Phong", "code": 3408, "division_type": "phường", "codename": "phuong_tan_phong", "province_code": 12 },
+      { "name": "Xã Bản Bo", "code": 3424, "division_type": "xã", "codename": "xa_ban_bo", "province_code": 12 },
+      { "name": "Xã Khun Há", "code": 3430, "division_type": "xã", "codename": "xa_khun_ha", "province_code": 12 },
+      { "name": "Xã Bum Tở", "code": 3433, "division_type": "xã", "codename": "xa_bum_to", "province_code": 12 },
+      { "name": "Xã Nậm Hàng", "code": 3434, "division_type": "xã", "codename": "xa_nam_hang", "province_code": 12 },
+      { "name": "Xã Thu Lũm", "code": 3439, "division_type": "xã", "codename": "xa_thu_lum", "province_code": 12 },
+    ]
+  },
+  {
+    provinceCode: '14',
+    wards: [
+      { "name": "Phường Tô Hiệu", "code": 3646, "division_type": "phường", "codename": "phuong_to_hieu", "province_code": 14 },
+      { "name": "Phường Chiềng An", "code": 3667, "division_type": "phường", "codename": "phuong_chieng_an", "province_code": 14 },
+      { "name": "Phường Chiềng Cơi", "code": 3670, "division_type": "phường", "codename": "phuong_chieng_coi", "province_code": 14 },
+      { "name": "Phường Chiềng Sinh", "code": 3679, "division_type": "phường", "codename": "phuong_chieng_sinh", "province_code": 14 },
+      { "name": "Xã Mường Chiên", "code": 3688, "division_type": "xã", "codename": "xa_muong_chien", "province_code": 14 },
+      { "name": "Xã Mường Giôn", "code": 3694, "division_type": "xã", "codename": "xa_muong_gion", "province_code": 14 },
+      { "name": "Xã Quỳnh Nhai", "code": 3703, "division_type": "xã", "codename": "xa_quynh_nhai", "province_code": 14 },
+      { "name": "Xã Mường Sại", "code": 3712, "division_type": "xã", "codename": "xa_muong_sai", "province_code": 14 },
+      { "name": "Xã Thuận Châu", "code": 3721, "division_type": "xã", "codename": "xa_thuan_chau", "province_code": 14 },
+      { "name": "Xã Bình Thuận", "code": 3724, "division_type": "xã", "codename": "xa_binh_thuan", "province_code": 14 },
+    ]
+  },
+  {
+    provinceCode: '15',
+    wards: [
+      { "name": "Phường Lào Cai", "code": 2647, "division_type": "phường", "codename": "phuong_lao_cai", "province_code": 15 },
+      { "name": "Phường Cam Đường", "code": 2674, "division_type": "phường", "codename": "phuong_cam_duong", "province_code": 15 },
+      { "name": "Xã Hợp Thành", "code": 2680, "division_type": "xã", "codename": "xa_hop_thanh", "province_code": 15 },
+      { "name": "Xã Bát Xát", "code": 2683, "division_type": "xã", "codename": "xa_bat_xat", "province_code": 15 },
+      { "name": "Xã A Mú Sung", "code": 2686, "division_type": "xã", "codename": "xa_a_mu_sung", "province_code": 15 },
+      { "name": "Xã Trịnh Tường", "code": 2695, "division_type": "xã", "codename": "xa_trinh_tuong", "province_code": 15 },
+      { "name": "Xã Y Tý", "code": 2701, "division_type": "xã", "codename": "xa_y_ty", "province_code": 15 },
+      { "name": "Xã Dền Sáng", "code": 2707, "division_type": "xã", "codename": "xa_den_sang", "province_code": 15 },
+      { "name": "Xã Bản Xèo", "code": 2725, "division_type": "xã", "codename": "xa_ban_xeo", "province_code": 15 },
+      { "name": "Xã Mường Hum", "code": 2728, "division_type": "xã", "codename": "xa_muong_hum", "province_code": 15 },
+    ]
+  },
+  {
+    provinceCode: '19',
+    wards: [
+      { "name": "Phường Đức Xuân", "code": 1840, "division_type": "phường", "codename": "phuong_duc_xuan", "province_code": 19 },
+      { "name": "Phường Bắc Kạn", "code": 1843, "division_type": "phường", "codename": "phuong_bac_kan", "province_code": 19 },
+      { "name": "Xã Phong Quang", "code": 1849, "division_type": "xã", "codename": "xa_phong_quang", "province_code": 19 },
+      { "name": "Xã Bằng Thành", "code": 1864, "division_type": "xã", "codename": "xa_bang_thanh", "province_code": 19 },
+      { "name": "Xã Cao Minh", "code": 1879, "division_type": "xã", "codename": "xa_cao_minh", "province_code": 19 },
+      { "name": "Xã Nghiên Loan", "code": 1882, "division_type": "xã", "codename": "xa_nghien_loan", "province_code": 19 },
+      { "name": "Xã Phúc Lộc", "code": 1894, "division_type": "xã", "codename": "xa_phuc_loc", "province_code": 19 },
+      { "name": "Xã Ba Bể", "code": 1906, "division_type": "xã", "codename": "xa_ba_be", "province_code": 19 },
+      { "name": "Xã Chợ Rã", "code": 1912, "division_type": "xã", "codename": "xa_cho_ra", "province_code": 19 },
+      { "name": "Xã Thượng Minh", "code": 1921, "division_type": "xã", "codename": "xa_thuong_minh", "province_code": 19 },
+    ]
+  },
+];
