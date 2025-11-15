180ef65f497b7d3557101ea2b4032350a20f35a7
annguyenbinh03 <phaolobinhan@gmail.com>
2025-11-15 13:17:10 +0700

    feat: call api getMentorForProject

--- src/hooks/api/users/index.ts
+++ src/hooks/api/users/index.ts
@@ -1,2 +1,2 @@
 export { userKeys } from './query-keys.ts'
-export { useGetUsers, useGetUserById, useGetMeQuery, useUpdateProfile, useUpdateUserRole, useUpdateUserStatus, useCreateUser } from './queries.ts'
+export { useGetUsers, useGetUserById, useGetMeQuery, useUpdateProfile, useUpdateUserRole, useUpdateUserStatus, useCreateUser, useGetMentorByProjectId } from './queries.ts'
