74918b46c17f91b773cb8cd669056a03d344ba6b
Lê Kim Bảo Nhật <lekimbaonhat.92.2017@gmail.com>
2025-11-30 12:08:27 +0700

    feat(project): implement role-based redirection for authenticated routes

--- src/routes/_authenticated/lab-admin/route.tsx
+++ src/routes/_authenticated/lab-admin/route.tsx
@@ -1,6 +1,19 @@
 import { AuthenticatedLayout } from "@/components/layout/authenticated-layout";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from '@tanstack/react-router'
+import { ROLE } from '@/const.ts'
+import { getRoleBasePath } from '@/lib/utils.ts'
 
 export const Route = createFileRoute('/_authenticated/lab-admin')({
-    component: AuthenticatedLayout
+  beforeLoad: ({ context }) => {
+    const user = context.authStore.getState().user
+
+    if (!user || user.role !== ROLE.LAB_ADMIN) {
+      const redirectPath = user?.role ? getRoleBasePath(user.role) : '/'
+
+      throw redirect({
+        to: redirectPath,
+      })
+    }
+  },
+  component: AuthenticatedLayout
 })
